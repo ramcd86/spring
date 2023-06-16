44be751f7ad6eecae9a9980eb7b7c25f49b47cdb
ramcd86 <ra_mcd@yahoo.com>
2023-06-16 09:39:40 +0100

    Updating ignore, removing generated files.

--- src/main/java/com/tradr/springboot/view/controllers/UserAuthorisationController.java
+++ src/main/java/com/tradr/springboot/view/controllers/UserAuthorisationController.java
@@ -18,127 +18,134 @@ import services.utils.UserEnums;
 @Controller
 public class UserAuthorisationController {
 
-  private final UserManagementService userManagementService;
-  private final StoreManagementService storeManagementService;
+	private final UserManagementService userManagementService;
+	private final StoreManagementService storeManagementService;
 
-  public UserAuthorisationController(
-    UserManagementService userManagementService,
-    StoreManagementService storeManagementService
-  ) {
-    this.userManagementService = userManagementService;
-    this.storeManagementService = storeManagementService;
-  }
+	public UserAuthorisationController(
+		UserManagementService userManagementService,
+		StoreManagementService storeManagementService
+	) {
+		this.userManagementService = userManagementService;
+		this.storeManagementService = storeManagementService;
+	}
 
-  @RequestMapping(value = "/user-login", method = RequestMethod.GET)
-  public String userPageLoginView(Model model) {
-    return "user-login";
-  }
+	@RequestMapping(value = "/user-login", method = RequestMethod.GET)
+	public String userPageLoginView(Model model) {
+		return "user-login";
+	}
 
-  @RequestMapping(value = "/user-login-verify", method = RequestMethod.POST)
-  public String userPageLogin(
-    @RequestParam String email,
-    @RequestParam String password,
-    Model model
-  ) {
-    model.addAttribute("email", email);
-    model.addAttribute("password", password);
+	@RequestMapping(value = "/user-login-verify", method = RequestMethod.POST)
+	public String userPageLogin(
+		@RequestParam String email,
+		@RequestParam String password,
+		Model model
+	) {
+		model.addAttribute("email", email);
+		model.addAttribute("password", password);
 
-    return "login-success";
-  }
+		return "login-success";
+	}
 
-  @PostMapping("auth")
-  public ResponseEntity<UserEnums> authoriseCurrentUser(
-    @RequestBody UserAuthKey userAuthKey
-  ) {
-    boolean userIsAuthed = userManagementService.isAuthKeyValid(userAuthKey);
-    return ResponseEntity.ok(
-      userIsAuthed ? UserEnums.USER_AUTHORISED : UserEnums.USER_NOT_AUTHORISED
-    );
-  }
+	@PostMapping("auth")
+	public ResponseEntity<UserEnums> authoriseCurrentUser(
+		@RequestBody UserAuthKey userAuthKey
+	) {
+		boolean userIsAuthed = userManagementService.isAuthKeyValid(
+			userAuthKey
+		);
+		return ResponseEntity.ok(
+			userIsAuthed
+				? UserEnums.USER_AUTHORISED
+				: UserEnums.USER_NOT_AUTHORISED
+		);
+	}
 
-  @PostMapping("login")
-  public ResponseEntity<PublicUserDetailsResponse> userLogin(
-    @RequestBody UserLoginDetails userLoginDetails
-  ) {
-    PublicUserDetailsResponse validatedPublicUserDetails = userManagementService.getUser(
-      userLoginDetails
-    );
-    return ResponseEntity.ok(validatedPublicUserDetails);
-  }
+	@PostMapping("login")
+	public ResponseEntity<PublicUserDetailsResponse> userLogin(
+		@RequestBody UserLoginDetails userLoginDetails
+	) {
+		PublicUserDetailsResponse validatedPublicUserDetails = userManagementService.getUser(
+			userLoginDetails
+		);
+		return ResponseEntity.ok(validatedPublicUserDetails);
+	}
 
-  @PostMapping("register")
-  public ResponseEntity<UserRegistrationResponse> createNewUser(
-    @RequestBody User user
-  ) {
-    UserRegistrationResponse userRegistrationResponse = new UserRegistrationResponse();
-    userRegistrationResponse.setUserRegistrationQueryStatus(
-      UserEnums.REGISTRATION_FAILED
-    );
-    user.setRegistrationDate(String.valueOf(LocalDate.now()));
-    List<StaticMaps.RegistrationFailureEnums> failureReasons = new ArrayList<StaticMaps.RegistrationFailureEnums>();
+	@PostMapping("register")
+	public ResponseEntity<UserRegistrationResponse> createNewUser(
+		@RequestBody User user
+	) {
+		UserRegistrationResponse userRegistrationResponse = new UserRegistrationResponse();
+		userRegistrationResponse.setUserRegistrationQueryStatus(
+			UserEnums.REGISTRATION_FAILED
+		);
+		user.setRegistrationDate(String.valueOf(LocalDate.now()));
+		List<StaticMaps.RegistrationFailureEnums> failureReasons = new ArrayList<StaticMaps.RegistrationFailureEnums>();
 
-    if (userManagementService.userExists(user.getEmail())) {
-      userRegistrationResponse.setUserRegistrationQueryStatus(
-        UserEnums.USER_EXISTS
-      );
-      return ResponseEntity.badRequest().body(userRegistrationResponse);
-    }
+		if (userManagementService.userExists(user.getEmail())) {
+			userRegistrationResponse.setUserRegistrationQueryStatus(
+				UserEnums.USER_EXISTS
+			);
+			return ResponseEntity.badRequest().body(userRegistrationResponse);
+		}
 
-    for (Field field : user.getClass().getDeclaredFields()) {
-      field.setAccessible(true);
-      try {
-        Object fieldValue = field.get(user);
-        if (
-          !field.getName().equals("authKey") &&
-          !field.getName().equals("authKeyExpiry") &&
-          !field.getName().equals("avatar") &&
-          !field.getName().equals("uuid") &&
-          !field.getName().equals("ownedStoreUUID")
-        ) {
-          if (fieldValue == null) {
-            failureReasons.add(
-              StaticMaps.registrationFailures.get(field.getName())
-            );
-          }
-        }
-      } catch (IllegalAccessException e) {
-        throw new RuntimeException(e);
-      }
-    }
+		for (Field field : user.getClass().getDeclaredFields()) {
+			field.setAccessible(true);
+			try {
+				Object fieldValue = field.get(user);
+				if (
+					!field.getName().equals("authKey") &&
+					!field.getName().equals("authKeyExpiry") &&
+					!field.getName().equals("avatar") &&
+					!field.getName().equals("uuid") &&
+					!field.getName().equals("ownedStoreUUID")
+				) {
+					if (fieldValue == null) {
+						failureReasons.add(
+							StaticMaps.registrationFailures.get(field.getName())
+						);
+					}
+				}
+			} catch (IllegalAccessException e) {
+				throw new RuntimeException(e);
+			}
+		}
 
-    if (!failureReasons.isEmpty()) {
-      userRegistrationResponse.setUserRegistrationQueryStatus(
-        UserEnums.REGISTRATION_FAILED
-      );
-      userRegistrationResponse.setUserRegistrationFailureConditions(
-        failureReasons
-      );
-      return ResponseEntity.badRequest().body(userRegistrationResponse);
-    } else {
-      boolean insertUser = userManagementService.insertUser(user);
-      if (insertUser) {
-        userRegistrationResponse.setUserRegistrationQueryStatus(
-          UserEnums.REGISTRATION_SUCCESSFUL
-        );
-        return ResponseEntity.ok(userRegistrationResponse);
-      } else {
-        userRegistrationResponse.setUserRegistrationQueryStatus(
-          UserEnums.REGISTRATION_FAILED
-        );
-        return ResponseEntity.badRequest().body(userRegistrationResponse);
-      }
-    }
-  }
+		if (!failureReasons.isEmpty()) {
+			userRegistrationResponse.setUserRegistrationQueryStatus(
+				UserEnums.REGISTRATION_FAILED
+			);
+			userRegistrationResponse.setUserRegistrationFailureConditions(
+				failureReasons
+			);
 
-  @PostMapping("delete-user")
-  public ResponseEntity<UserEnums> deleteExistingUser(
-    @RequestBody UserAuthKey userAuthKey
-  ) {
-    UserEnums deleteUserResponse = userManagementService.deleteUser(
-      userAuthKey,
-      storeManagementService
-    );
+			return ResponseEntity.badRequest().body(userRegistrationResponse);
+		} else {
+			boolean insertUser = userManagementService.insertUser(user);
+			if (insertUser) {
+				userRegistrationResponse.setUserRegistrationQueryStatus(
+					UserEnums.REGISTRATION_SUCCESSFUL
+				);
+				return ResponseEntity.ok(userRegistrationResponse);
+			} else {
+				userRegistrationResponse.setUserRegistrationQueryStatus(
+					UserEnums.REGISTRATION_FAILED
+				);
+				return ResponseEntity
+					.badRequest()
+					.body(userRegistrationResponse);
+			}
+		}
+	}
 
-    return ResponseEntity.status(HttpStatus.OK).body(deleteUserResponse);
-  }
+	@PostMapping("delete-user")
+	public ResponseEntity<UserEnums> deleteExistingUser(
+		@RequestBody UserAuthKey userAuthKey
+	) {
+		UserEnums deleteUserResponse = userManagementService.deleteUser(
+			userAuthKey,
+			storeManagementService
+		);
+
+		return ResponseEntity.status(HttpStatus.OK).body(deleteUserResponse);
+	}
 }
